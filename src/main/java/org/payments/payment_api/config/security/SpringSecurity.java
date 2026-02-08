794cc3c4acd65be99054c762ad0b67515caff7ba
lucasoliveira04 <lucasolisocialmedia@gmail.com>
2026-02-08 18:44:09 -0300

    feat: implement security configuration for dev and non-dev profiles, and add payment timeout logging functionality

--- src/main/java/org/payments/payment_api/config/security/SpringSecurity.java
+++ src/main/java/org/payments/payment_api/config/security/SpringSecurity.java
@@ -1,4 +1,22 @@
 package org.payments.payment_api.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+@Configuration
+@Profile("!dev")
 public class SpringSecurity {
+
+    @Bean
+    public DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                ).build();
+    }
+
 }
